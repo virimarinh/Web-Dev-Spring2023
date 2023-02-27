49b33288afd7e161a6734eaba6fc05772ec22850
Viri Marin <marinhev1@newpaltz.edu>
2023-02-27 16:42:33 -0500

    updated App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
-
-
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <NavBar />
+    
+    <NavBar />
 
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="New Paltz!" /> -->
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="container">
+        <RouterView />
     </div>
-  </header>
-
-  <RouterView />
+    
 </template>
 
 <style scoped>
