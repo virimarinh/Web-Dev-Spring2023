b1600b83e267f64229f4582d079f77574e5c4e3c
Viri Marin <marinhev1@newpaltz.edu>
2023-03-23 17:48:41 -0400

    asynchronous programming

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const jokes = require('./controllers/products');
 const products = require('./controllers/products')
 const app = express()
 
@@ -17,13 +18,19 @@ app
     .get('api/v1', (req, res) => {
         res.send('Hello World! From Express')
     })
-    .use('api/v1/products', products)
+    .use('/api/v1/products', products)
+    .use('/api/v1/jokes', jokes)
 
 // Catch all
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 })
 
+
+console.log('1: About to start server')
+
     app.listen(port, () => 
-      console.log(`Server running at http://${hostname}:${port}/`)
+      console.log(`2: Server running at http://${hostname}:${port}/`)
       );
+
+      console.log('3: Asked server to start')
