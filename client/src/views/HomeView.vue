d33c3b99fd18244d1f304806bbfa9d0ae6fe3b39
Viri Marin <marinhev1@newpaltz.edu>
2023-04-10 22:40:12 -0400

    creating a reusable Modal Dialog

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
+import Modal from '@/components/Modal.vue';
+import { ref } from 'vue';
+    const isOpen = ref(false);
 </script>
 
 <template>
   <main>
     <h1 class="title">
-    This is our home page
-  </h1>
+        This is our home page
+    </h1>
+
+    <p>
+        <button class="button" @click="isOpen = !isOpen">Open Modal</button>
+    </p>
+
+    <Modal v-model:is-open="isOpen" >
+        <h1 class="title">Some Content in the slot</h1>
+    </Modal>
   </main>
 </template>
