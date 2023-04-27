628347ebbd3c4e3a4cbd7ea68e9018d89bcb3112
Viri Marin <marinhev1@newpaltz.edu>
2023-04-27 14:35:02 -0400

    json web tokens

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -3,63 +3,71 @@ const model = require('../models/products');
 const router = express.Router();
 
 router
-    .get('/', (req, res) => {
+    .get('/', (req, res, next) => {
         model.getAll(+req.query.page, +req.query.pageSize)
-        .then(list => {
-            const data = { data: list.items, total: list.total, isSuccess: true };
-            res.send(data)
-        }).catch(next)
+            .then(list => {
+                const data = { data: list.items, total: list.total, isSuccess: true };
+                res.send(data)
+            }).catch(next);
+    })
+
+    .get('/purchases', (req, res, next) => {
+        model.getAll(+req.query.page, +req.query.pageSize)
+            .then(list => {
+                const data = { data: list.items, total: list.total, isSuccess: true };
+                res.send(data)
+            }).catch(next);
     })
 
     .get('/search/:q', (req, res, next) => {
 
         model.search(req.params.q, +req.query.page, +req.query.pageSize)
-        .then(list => {
-            const data = { data: list.items, total: list.total, isSuccess: true };
-            res.send(data)
-    }).catch(next);
-
-})
+            .then(list => {
+                const data = { data: list.items, total: list.total, isSuccess: true };
+                res.send(data)
+            }).catch(next);
+        
+    })
 
     .get('/:id', (req, res, next) => {
-        
-        model.get(req.params.id)
-        .then(item => {
-            const data = { data: XMLDocument, isSuccess: true };
-            res.send(data)
-    }).catch(next);
 
- })
+        model.getById(req.params.id)
+            .then(x => {
+                const data = { data: x, isSuccess: true };
+                res.send(data)
+            }).catch(next);
 
-    .post('/', (req, res) => {
+    })
 
-        model.create(req.body)
-        .then(item => {
-            const data = { data: x, isSuccess: true };
-            res.send(data)
-            }).catch(next)
+    .post('/', (req, res, next) => {
+
+        model.add(req.body)
+            .then(x => {
+                const data = { data: x, isSuccess: true };
+                res.send(data)
+            }).catch(next);
 
     })
 
     .patch('/', (req, res, next) => {
 
-       model.update(req.body)
-       .then(item => {
-        const data = { data: x, isSuccess: true };
-        res.send(data)
-        }).catch(next)
+        model.update(req.body)
+            .then(x => {
+                const data = { data: x, isSuccess: true };
+                res.send(data)
+            }).catch(next);
 
     })
 
     .delete('/:id', (req, res, next) => {
 
-        model.delete(req.params.id)
-        .then(item => { 
-            const data = { data: x, isSuccess: true };
-            res.send(data)
-            }).catch(next)
+        model.deleteItem(req.params.id)
+            .then(x => {
+                const data = { data: x, isSuccess: true };
+                res.send(data)
+            }).catch(next);
     })
-    
+
     .post('/seed', (req, res, next) => {
         model.seed()
             .then(x => {
@@ -67,8 +75,6 @@ router
                 res.send(data)
             }).catch(next);
     });
-
-    
 
 module.exports = router;
 
